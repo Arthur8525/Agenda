ff45f82af4d69589f67f9db35a349e39e95e7659
Arthur <115493229+Arthur8525@users.noreply.github.com>
2025-03-14 20:52:38 -0300

    Delete ProjetoBiblioteca directory

--- ProjetoBiblioteca/livros/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Livro
-
-@admin.register(Livro)
-class LivroAdmin(admin.ModelAdmin):
-    list_display = ('titulo', 'autor', 'ano_publicacao')
-
-# Register your models here.
--- ProjetoBiblioteca/livros/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from .models import Livro
-
-def lista_livros(request):
-    livros = Livro.objects.all()
-    return render(request, 'livros/index.html')
-
-
-# Create your views here.
